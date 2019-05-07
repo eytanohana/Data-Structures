9187b7e72014ba017a26c9fae950a814183fa00c
Eytan Ohana <eohana98@gmail.com>
2019-05-07 21:25:30 +0300

    Deleted Main

--- lists/LinkedList.java
+++ lists/LinkedList.java
@@ -234,5 +234,40 @@ public class LinkedList<T> implements ListInterface<T>{
 		sb.append("\\\\");
 		return sb.toString();
 	}
+	
+
+
+	public static void main(String[] args) {
+		LinkedList<Integer> ls = new LinkedList<>();
+
+		for (int i = 0; i < 10; i++)
+			ls.add(i);
+
+		System.out.println(ls);
+
+		ls.add(6, 0);
+		System.out.println(ls);
+
+		ls.add(90 ,11);
+		System.out.println(ls);
+
+		System.out.println(ls.contains(11));
+
+		System.out.println(ls.get(10));
+
+		System.out.println(ls.indexOf(9));
+
+		System.out.println(ls);
+		ls.remove(0);
+		System.out.println(ls);
+
+
+		ls.remove(0);
+		System.out.println(ls);
+		System.out.println(ls.size());
+
+		System.out.println(ls.getLast());
+		System.out.println(ls.getFirst());
+	}
 
 }
