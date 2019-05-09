93dc6f57933fceab300aa3e0e4d50970bc749d8d
Eytan Ohana <eohana98@gmail.com>
2019-05-09 22:06:23 +0300

    Started over BST. Trying to define it recursively.

--- trees/BinarySearchTree.java
+++ trees/BinarySearchTree.java
@@ -1,51 +1,21 @@
 package trees;
 
-public class BinarySearchTree<T> {
+public class BinarySearchTree<T extends Comparable<T>> {
 
-    class Node<T> {
-        private T data;
-        private Node<T> leftChild;
-        private Node<T> rightChild;
+    private BinarySearchTree<T> left;
+    private BinarySearchTree<T> right;
 
-        public Node(T data, Node<T> left, Node<T> right) {
-            this.data = data;
-            this.leftChild = left;
-            this.rightChild = right;
-        }
-
-        public Node(T data) {
-            this(data, null, null);
-        }
-
-
-        public T getData() {
-            return this.data;
-        }
-
-        public Node<T> getLeftChild() {
-            return leftChild;
-        }
-
-        public Node<T> getRightChild() {
-            return rightChild;
-        }
-
-        public void setLeftChild(Node<T> left) {
-            leftChild = left;
-        }
-
-        public void setRightChild(Node<T> right) {
-            rightChild = right;
-        }
-    }
-
-    private Node<T> root;
+    private T data;
     private int size;
 
     public BinarySearchTree(T data) {
-        root = new Node(data);
-        size = 0;
+        this.data = data;
+        left = null;
+        right = null;
     }
+
+
+
 
     public static void main(String[] args) {
 
