da47c8f58dd812b5210034a0cbb738aabee0b6c4
Eytan Ohana <eohana98@gmail.com>
2019-05-23 16:36:32 +0300

    Added a Main class to test everything

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import trees.BinarySearchTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+//           Build a tree that looks like:
+        //
+//                      5
+//                    /   \
+//                   0     10
+//                 /   \  /  \
+//                -5   3 7    15
+
+
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>(5);
+
+        tree.insert(10);
+        tree.insert(0);
+        tree.insert(-5);
+        tree.insert(3);
+        tree.insert(7);
+        tree.insert(15);
+
+        tree.printTree();
+
+        System.out.println();
+        System.out.println();
+
+        tree.inOrderTraversal();
+        System.out.println();
+        tree.preOrderTraversal();
+        System.out.println();
+        tree.postOrderTraversal();
+    }
+
+}
--- src/trees/BinarySearchTree.java
+++ src/trees/BinarySearchTree.java
@@ -151,36 +151,4 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
 
     }
-
-
-    public static void main(String[] args) {
-//           Build a tree that looks like:
- //
-//                      5
-//                    /   \
-//                   0     10
-//                 /   \  /  \
-//                -5   3 7    15
-
-
-        BinarySearchTree<Integer> tree = new BinarySearchTree<>(5);
-
-        //tree.insert(10);
-        tree.insert(0);
-        tree.insert(-5);
-        tree.insert(3);
-        tree.insert(7);
-        //tree.insert(15);
-
-        tree.printTree();
-
-        System.out.println();
-        System.out.println();
-
-        tree.inOrderTraversal();
-        System.out.println();
-        tree.preOrderTraversal();
-        System.out.println();
-        tree.postOrderTraversal();
-    }
 }
