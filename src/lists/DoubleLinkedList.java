9e9afd1992c032802684a5bbbde6ed4f8a6c6333
eytan-ohana <eytan.ohana@traxretail.com>
2021-02-19 17:53:39 +0200

    add unimplemented methods from interface

--- src/lists/DoubleLinkedList.java
+++ src/lists/DoubleLinkedList.java
@@ -1,16 +1,69 @@
 package lists;
 
-public class DoubleLinkedList<T> extends LinkedList<T> {
+public class DoubleLinkedList<T> implements ListInterface<T> {
 
-    private Node<T> last;
+    private DoubleNode<T> head;
+    private DoubleNode<T> tail;
 
     public DoubleLinkedList () {
-        // creates a head with null and size 0 from the LinkedList constructor
-        // (super must be first line of constructor)
-        super();
-        last = new Node(null, head, null);
-        head.setNext(last);
 
     }
 
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public boolean add(T element, int index) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(T element) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void remove(T element) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+        DoubleLinkedList<Integer> dls = new DoubleLinkedList<>();
+
+        System.out.println(dls);
+    }
 }
